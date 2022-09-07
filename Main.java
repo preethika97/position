a01fa659b877732be6c4c6906003c92b7c7291f7
Preethika <112964321+preethika97@users.noreply.github.com>
2022-09-07 14:22:59 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	int a=1,n=60,i;
+	Scanner sc=new Scanner(System.in);
+	int b= sc.nextInt();
+	for(i=1;i<=b;i++)
+	{
+	    a=a*2;
+	}
+	System.out.println(a);
+	}
+}
